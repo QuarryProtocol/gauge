6a8d8cf7f31ec7f95275517b6557cc557ef16796
Larry Jarry <larry@quarry.so>
2022-04-28 05:03:07 +0800

    Better IDL export

--- src/programs/gauge.ts
+++ src/programs/gauge.ts
@@ -1,3 +1,4 @@
+import type { Program } from "@project-serum/anchor";
 import type { AnchorTypes } from "@saberhq/anchor-contrib";
 
 import type { GaugeIDL } from "../idls/gauge";
@@ -19,12 +20,12 @@ export type GaugeTypes = AnchorTypes<
 
 type Accounts = GaugeTypes["Accounts"];
 
-export type GaugemeisterData = Accounts["Gaugemeister"];
-export type GaugeData = Accounts["Gauge"];
-export type GaugeVoterData = Accounts["GaugeVoter"];
-export type GaugeVoteData = Accounts["GaugeVote"];
-export type EpochGaugeData = Accounts["EpochGauge"];
-export type EpochGaugeVoterData = Accounts["EpochGaugeVoter"];
-export type EpochGaugeVoteData = Accounts["EpochGaugeVote"];
+export type GaugemeisterData = Accounts["gaugemeister"];
+export type GaugeData = Accounts["gauge"];
+export type GaugeVoterData = Accounts["gaugeVoter"];
+export type GaugeVoteData = Accounts["gaugeVote"];
+export type EpochGaugeData = Accounts["epochGauge"];
+export type EpochGaugeVoterData = Accounts["epochGaugeVoter"];
+export type EpochGaugeVoteData = Accounts["epochGaugeVote"];
 
-export type GaugeProgram = GaugeTypes["Program"];
+export type GaugeProgram = Program<GaugeIDL>;
