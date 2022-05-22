167e2715ea28ee8f5bc48c80764691cbfd52267b
Ian Macalinao <ian@saber.so>
2022-05-21 22:27:24 -0500

    add sync versions of pda generators

--- src/wrappers/gauge/pda.ts
+++ src/wrappers/gauge/pda.ts
@@ -1,4 +1,5 @@
 import { utils } from "@project-serum/anchor";
+import { getProgramAddress } from "@saberhq/solana-contrib";
 import { PublicKey } from "@solana/web3.js";
 
 import { GAUGE_ADDRESSES } from "../../constants";
@@ -114,6 +115,118 @@ export const findEpochGaugeVoteAddress = async (
   votingEpoch: number
 ): Promise<[PublicKey, number]> => {
   return await PublicKey.findProgramAddress(
+    [
+      utils.bytes.utf8.encode("EpochGaugeVote"),
+      gaugeVote.toBuffer(),
+      encodeU32(votingEpoch),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of a GaugeVoter.
+ */
+export const getGaugeVoterAddress = (
+  gaugemeister: PublicKey,
+  escrow: PublicKey
+): PublicKey => {
+  return getProgramAddress(
+    [
+      utils.bytes.utf8.encode("GaugeVoter"),
+      gaugemeister.toBuffer(),
+      escrow.toBuffer(),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of a Gaugemeister.
+ */
+export const getGaugemeisterAddress = (base: PublicKey): PublicKey => {
+  return getProgramAddress(
+    [utils.bytes.utf8.encode("Gaugemeister"), base.toBuffer()],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of a Gauge.
+ */
+export const getGaugeAddress = (
+  gaugemeister: PublicKey,
+  quarry: PublicKey
+): PublicKey => {
+  return getProgramAddress(
+    [
+      utils.bytes.utf8.encode("Gauge"),
+      gaugemeister.toBuffer(),
+      quarry.toBuffer(),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of a GaugeVote.
+ */
+export const getGaugeVoteAddress = (
+  gaugeVoter: PublicKey,
+  gauge: PublicKey
+): PublicKey => {
+  return getProgramAddress(
+    [
+      utils.bytes.utf8.encode("GaugeVote"),
+      gaugeVoter.toBuffer(),
+      gauge.toBuffer(),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of an epoch gauge.
+ */
+export const getEpochGaugeAddress = (
+  gauge: PublicKey,
+  votingEpoch: number
+): PublicKey => {
+  return getProgramAddress(
+    [
+      utils.bytes.utf8.encode("EpochGauge"),
+      gauge.toBuffer(),
+      encodeU32(votingEpoch),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of an epoch gauge voter.
+ */
+export const getEpochGaugeVoterAddress = (
+  gaugeVoter: PublicKey,
+  votingEpoch: number
+): PublicKey => {
+  return getProgramAddress(
+    [
+      utils.bytes.utf8.encode("EpochGaugeVoter"),
+      gaugeVoter.toBuffer(),
+      encodeU32(votingEpoch),
+    ],
+    GAUGE_ADDRESSES.Gauge
+  );
+};
+
+/**
+ * Finds the address of an epoch gauge vote.
+ */
+export const getEpochGaugeVoteAddress = (
+  gaugeVote: PublicKey,
+  votingEpoch: number
+): PublicKey => {
+  return getProgramAddress(
     [
       utils.bytes.utf8.encode("EpochGaugeVote"),
       gaugeVote.toBuffer(),
